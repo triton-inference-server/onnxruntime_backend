8e6a62c166321852525f7c26e69038677641d959
Chun-Wei Chen <jacky82226@gmail.com>
2021-09-28 14:13:16 -0700

    refactor code

--- src/onnxruntime_utils.h
+++ src/onnxruntime_utils.h
@@ -37,50 +37,46 @@ namespace triton { namespace backend { namespace onnxruntime {
 
 extern const OrtApi* ort_api;
 
-#define RESPOND_ALL_AND_RETURN_IF_ERROR(                            \
-    REQUESTS, REQUEST_COUNT, RESPONSES, S)                          \
-  do {                                                              \
-    TRITONSERVER_Error* raarie_err__ = (S);                         \
-    if (raarie_err__ != nullptr) {                                  \
-      SendErrorForResponses(RESPONSES, REQUEST_COUNT, raarie_err__);\
-      for (uint32_t r = 0; r < REQUEST_COUNT; ++r) {                \
-        if (RESPONSES[r] != nullptr) {                              \
-        LOG_IF_ERROR(                                               \
-            TRITONBACKEND_RequestRelease(                           \
-                REQUESTS[r], TRITONSERVER_REQUEST_RELEASE_ALL),     \
-            "failed releasing request");                            \
-        REQUESTS[r] = nullptr;                                      \
-        }                                                           \
-      }                                                             \
-      return;                                                       \
-    }                                                               \
+#define RESPOND_ALL_AND_RETURN_IF_ERROR(                          \
+    REQUESTS, REQUEST_COUNT, RESPONSES, S)                        \
+  do {                                                            \
+    TRITONSERVER_Error* raarie_err__ = (S);                       \
+    if (raarie_err__ != nullptr) {                                \
+      for (uint32_t r = 0; r < REQUEST_COUNT; ++r) {              \
+        TRITONBACKEND_Response* response = (*RESPONSES)[r];       \
+        if (response != nullptr) {                                \
+          LOG_IF_ERROR(                                           \
+              TRITONBACKEND_ResponseSend(                         \
+                  response, TRITONSERVER_RESPONSE_COMPLETE_FINAL, \
+                  raarie_err__),                                  \
+              "failed to send ONNXRuntime backend response");     \
+        }                                                         \
+        LOG_IF_ERROR(                                             \
+            TRITONBACKEND_RequestRelease(                         \
+                REQUESTS[r], TRITONSERVER_REQUEST_RELEASE_ALL),   \
+            "failed releasing request");                          \
+        REQUESTS[r] = nullptr;                                    \
+      }                                                           \
+      TRITONSERVER_ErrorDelete(raarie_err__);                     \
+      return;                                                     \
+    }                                                             \
   } while (false) 
 
-#define RESPOND_ALL_AND_RETURN_IF_ORT_ERROR(                               \
-    REQUESTS, REQUEST_COUNT, RESPONSES, S)                                 \
-  do {                                                                     \
-    OrtStatus* status__ = (S);                                             \
-    if (status__ != nullptr) {                                             \
-      OrtErrorCode code = ort_api->GetErrorCode(status__);                 \
-      std::string msg = std::string(ort_api->GetErrorMessage(status__));   \
-      ort_api->ReleaseStatus(status__);                                    \
-      auto err__ = TRITONSERVER_ErrorNew(                                  \
-              TRITONSERVER_ERROR_INTERNAL,                                 \
-              (std::string("onnx runtime error ") + std::to_string(code) + \
-               ": " + msg)                                                 \
-                  .c_str());                                               \
-      SendErrorForResponses(RESPONSES, REQUEST_COUNT, err__);              \
-      for (uint32_t r = 0; r < REQUEST_COUNT; ++r) {                       \
-        if (RESPONSES[r] != nullptr) {                                     \
-        LOG_IF_ERROR(                                                      \
-            TRITONBACKEND_RequestRelease(                                  \
-                REQUESTS[r], TRITONSERVER_REQUEST_RELEASE_ALL),            \
-            "failed releasing request");                                   \
-        REQUESTS[r] = nullptr;                                             \
-        }                                                                  \
-      }                                                                    \
-      return;                                                              \
-    }                                                                      \
+#define RESPOND_ALL_AND_RETURN_IF_ORT_ERROR(                                      \
+    REQUESTS, REQUEST_COUNT, RESPONSES, S)                                        \
+  do {                                                                            \
+    OrtStatus* status__ = (S);                                                    \
+    if (status__ != nullptr) {                                                    \
+      OrtErrorCode code = ort_api->GetErrorCode(status__);                        \
+      std::string msg = std::string(ort_api->GetErrorMessage(status__));          \
+      ort_api->ReleaseStatus(status__);                                           \
+      auto err__ = TRITONSERVER_ErrorNew(                                         \
+              TRITONSERVER_ERROR_INTERNAL,                                        \
+              (std::string("onnx runtime error ") + std::to_string(code) +        \
+               ": " + msg)                                                        \
+                  .c_str());                                                      \
+      RESPOND_ALL_AND_RETURN_IF_ERROR(REQUESTS, REQUEST_COUNT, RESPONSES, err__); \
+    }                                                                             \
   } while (false)
 
 #define RETURN_IF_ORT_ERROR(S)                                               \
