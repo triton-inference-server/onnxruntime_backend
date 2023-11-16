23c81d23945d1fd6176240a63891e1ba36642817
krishung5 <krish@nvidia.com>
2023-11-15 19:50:00 -0800

    Testing

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -54,7 +54,8 @@ def dockerfile_common():
     df = """
 ARG BASE_IMAGE={}
 ARG ONNXRUNTIME_VERSION={}
-ARG ONNXRUNTIME_REPO=https://github.com/microsoft/onnxruntime
+# ARG ONNXRUNTIME_REPO=https://github.com/microsoft/onnxruntime
+ARG ONNXRUNTIME_REPO=https://github.com/krishung5/onnxruntime
 ARG ONNXRUNTIME_BUILD_CONFIG={}
 """.format(
         FLAGS.triton_container, FLAGS.ort_version, FLAGS.ort_build_config
