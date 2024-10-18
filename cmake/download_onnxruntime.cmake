bc8b479a4d37805c1f1fda2b092c427a82b99533
Misha Chornyi <mchornyi@nvidia.com>
2024-10-17 19:07:51 -0700

    Switch to argument vs variable

--- cmake/download_onnxruntime.cmake
+++ cmake/download_onnxruntime.cmake
@@ -1,18 +1,18 @@
 if(WIN32)
 
-  message(NOTICE "Downloading onnxruntime: $ENV{TRITON_ONNXRUNTIME_PACKAGE_URL}")
+  message(NOTICE "Downloading onnxruntime: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
 
   # Define the download and extraction paths)
   set(DOWNLOAD_PATH "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime.zip")
   set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime")
 
   # Download the file
-  file(DOWNLOAD $ENV{TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
+  file(DOWNLOAD ${TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
 
   # Check the download status
   list(GET DOWNLOAD_STATUS 0 DOWNLOAD_RESULT)
   if(NOT DOWNLOAD_RESULT EQUAL 0)
-      message(SEND_ERROR "Failed to download ${ONNXRUNTIME_PACKAGE_URL}")
+      message(SEND_ERROR "Failed to download: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
   else()
       message(NOTICE "Download successful: ${DOWNLOAD_PATH}" )
 
