2d85b9a056e4a4e9e97940ae1480c9560a5feeef
Misha Chornyi <mchornyi@nvidia.com>
2024-10-24 07:28:51 -0700

    Add logic to pull Onnx Runtime

--- cmake/download_onnxruntime.cmake
+++ cmake/download_onnxruntime.cmake
@@ -1,27 +1,25 @@
-if(WIN32)
+# Define the download and extraction paths)
+set(DOWNLOAD_PATH "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime.zip")
+set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime")
 
-  message(NOTICE "Downloading onnxruntime: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
+if(DEFINED TRITON_ONNXRUNTIME_PACKAGE_URL)
+    message(NOTICE "Downloading onnxruntime: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
+    # Download the file
+    file(DOWNLOAD ${TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
 
-  # Define the download and extraction paths)
-  set(DOWNLOAD_PATH "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime.zip")
-  set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime")
 
-  # Download the file
-  file(DOWNLOAD ${TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
+    # Check the download status
+    list(GET DOWNLOAD_STATUS 0 DOWNLOAD_RESULT)
+    if(NOT DOWNLOAD_RESULT EQUAL 0)
+        message(NOTICE "Failed to download: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
+    else()
+        message(NOTICE "Download successful: ${DOWNLOAD_PATH}" )
 
-  # Check the download status
-  list(GET DOWNLOAD_STATUS 0 DOWNLOAD_RESULT)
-  if(NOT DOWNLOAD_RESULT EQUAL 0)
-      message(SEND_ERROR "Failed to download: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
-  else()
-      message(NOTICE "Download successful: ${DOWNLOAD_PATH}" )
+        # Extract the downloaded file
+        file(ARCHIVE_EXTRACT INPUT ${DOWNLOAD_PATH} DESTINATION ${EXTRACT_DIR} VERBOSE )
 
-      # Extract the downloaded file
-      file(ARCHIVE_EXTRACT INPUT ${DOWNLOAD_PATH} DESTINATION ${EXTRACT_DIR} VERBOSE )
+        set(TRITON_ONNXRUNTIME_INCLUDE_PATHS ${EXTRACT_DIR}/include)
+        set(TRITON_ONNXRUNTIME_LIB_PATHS ${EXTRACT_DIR}/lib)
 
-      set(TRITON_ONNXRUNTIME_INCLUDE_PATHS ${EXTRACT_DIR}/include)
-      set(TRITON_ONNXRUNTIME_LIB_PATHS ${EXTRACT_DIR}/lib)
-
-  endif(NOT DOWNLOAD_RESULT EQUAL 0)
-
-endif(WIN32)
+    endif(NOT DOWNLOAD_RESULT EQUAL 0)
+endif(DEFINED TRITON_ONNXRUNTIME_PACKAGE_URL)
