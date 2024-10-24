3a1fded40e2282a1b8e9792046f3b7a7bffe027e
Misha Chornyi <mchornyi@nvidia.com>
2024-10-24 07:35:01 -0700

    Formating configuration

--- cmake/download_onnxruntime.cmake
+++ cmake/download_onnxruntime.cmake
@@ -1,25 +1,29 @@
-# Define the download and extraction paths)
-set(DOWNLOAD_PATH "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime.zip")
-set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime")
-
 if(DEFINED TRITON_ONNXRUNTIME_PACKAGE_URL)
-    message(NOTICE "Downloading onnxruntime: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
-    # Download the file
-    file(DOWNLOAD ${TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
+  # Define the download and extraction paths)
+  set(DOWNLOAD_PATH "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime.zip")
+  set(EXTRACT_DIR "${CMAKE_BINARY_DIR}/_deps/downloads/onnxruntime")
 
+  message(NOTICE "Downloading onnxruntime: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
 
-    # Check the download status
-    list(GET DOWNLOAD_STATUS 0 DOWNLOAD_RESULT)
-    if(NOT DOWNLOAD_RESULT EQUAL 0)
-        message(NOTICE "Failed to download: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
-    else()
-        message(NOTICE "Download successful: ${DOWNLOAD_PATH}" )
+  # Download the file
+  file(DOWNLOAD ${TRITON_ONNXRUNTIME_PACKAGE_URL} ${DOWNLOAD_PATH} SHOW_PROGRESS STATUS DOWNLOAD_STATUS)
 
-        # Extract the downloaded file
-        file(ARCHIVE_EXTRACT INPUT ${DOWNLOAD_PATH} DESTINATION ${EXTRACT_DIR} VERBOSE )
+  # Check the download status
+  list(GET DOWNLOAD_STATUS 0 DOWNLOAD_RESULT)
 
-        set(TRITON_ONNXRUNTIME_INCLUDE_PATHS ${EXTRACT_DIR}/include)
-        set(TRITON_ONNXRUNTIME_LIB_PATHS ${EXTRACT_DIR}/lib)
+  # Extract the downloaded file if the download was successful
+  if(NOT DOWNLOAD_RESULT EQUAL 0)
+    message(NOTICE "Failed to download: ${TRITON_ONNXRUNTIME_PACKAGE_URL}")
+  else()
+    message(NOTICE "Download successful: ${DOWNLOAD_PATH}" )
 
-    endif(NOT DOWNLOAD_RESULT EQUAL 0)
+    # Extract the downloaded file
+    file(ARCHIVE_EXTRACT INPUT ${DOWNLOAD_PATH} DESTINATION ${EXTRACT_DIR} VERBOSE )
+
+    # Update CMakeLists.txt configuration references with new values
+    set(TRITON_ONNXRUNTIME_INCLUDE_PATHS ${EXTRACT_DIR}/include)
+    set(TRITON_ONNXRUNTIME_LIB_PATHS ${EXTRACT_DIR}/lib)
+
+  endif(NOT DOWNLOAD_RESULT EQUAL 0)
+
 endif(DEFINED TRITON_ONNXRUNTIME_PACKAGE_URL)
