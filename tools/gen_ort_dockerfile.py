e45ae542da755aee6082c4d40b0d0c75ce69724c
Misha Chornyi <mchornyi@nvidia.com>
2026-02-23 15:16:09 -0800

    Use openssl3 in manylinux

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -184,7 +184,7 @@ RUN dnf install -y \\
         curl \\
         git \\
         gnupg \\
-        openssl-devel \\
+        openssl3-devel \\
         python3-pip \\
         wget \\
         zip
